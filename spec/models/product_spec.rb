7c2a3467a49e2544fa03a08b4d0016d7eb53c7e0
anderCM <andercm15@gmail.com>
2023-06-16 14:51:14 -0500

    Unit tests for Reservation model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it 'is invalid without name' do
-    product = Product.new()
+    product = Product.new
     product.valid?
     expect(product.errors[:name]).to include("can't be blank")
   end
@@ -22,13 +22,13 @@ RSpec.describe Product, type: :model do
   it 'is invalid with prices lower than 0' do
     product = Product.new(name: 'Gun 1', image: 'https://urlpicture.com', price: -1)
     product.valid?
-    expect(product.errors[:price]).to include("must be greater than or equal to 0")
+    expect(product.errors[:price]).to include('must be greater than or equal to 0')
   end
 
   it 'is invalid without owner' do
     product = Product.new(name: 'Gun 1', image: 'https://urlpicture.com', price: 27.20)
     product.valid?
-    expect(product.errors[:owner]).to include("must exist")
+    expect(product.errors[:owner]).to include('must exist')
   end
 
   it 'should be stored in DB' do
